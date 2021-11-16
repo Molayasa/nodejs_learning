2c91225e7c7874a6c0b797658175c5bdd48543a4
molayasa <paulinaolaya1@gmail.com>
2021-11-16 11:59:56 -0500

    Use a ternary operator for check a command line argument

--- learning.js
+++ learning.js
@@ -1,1 +1,11 @@
-console.log("Node is working!!!");
+const word = process.argv[2];
+
+// if (word === 'learn') {
+//   console.log(`It's time to ${word}!`);
+// } else {
+//   console.log(`Let's ${word} and rock!`);
+// }
+
+word === 'learn'
+  ? console.log(`It's time to ${word}!`)
+  : console.log(`Let's ${word} and rock!`);
